07fff7da222896c6f11ad37a0f1e32cff63f9c5c
svanmarc <stefaniavanmarcke@gmail.com>
2024-09-25 14:19:40 +0200

    sup_comment

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Detect the installed PHP version (e.g., 8.0, 8.2)
 PHP_VERSION=$(php -v | head -n 1 | grep -oP '(?<=PHP )\d+\.\d+')
 
-# Modify the PHP-FPM configuration to listen on port 9000
 sed -i -e "s/.*listen = .*/listen = 9000/" /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
 
-# Check if essential environment variables are set
 if [[ -z "$WORDPRESS_DB" || -z "$WORDPRESS_USER" || -z "$WORDPRESS_PASSWORD" || -z "$WORDPRESS_DB_HOST" || -z "$WP_EMAIL" ]]; then
     echo "Error: Missing essential environment variables."
     echo "WORDPRESS_DB, WORDPRESS_USER, WORDPRESS_PASSWORD, WORDPRESS_DB_HOST, and WP_EMAIL must be set."
     exit 1
 fi
 
-# Download and configure WordPress if it has not been set up yet
 if [ ! -f wp-config.php ]; then
     echo "Downloading WordPress..."
     wp core download --allow-root
@@ -30,5 +26,4 @@ else
     echo "WordPress already installed. Skipping setup."
 fi
 
-# Start PHP-FPM in the foreground with the detected PHP version
 exec /usr/sbin/php-fpm${PHP_VERSION} -F
